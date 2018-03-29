42c8af3ce5c3314b74e8c20c24e65cdf59141f3b
优里 <SoulCrash@yandex.ru>
2018-03-29 11:13:10 +0800

    Added rosrc for "EDF Central (Linux)"

--- sshd.sh
+++ sshd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 xrandr --newmode "1656x900_60.00" 122.25 1656 1752 1920 2184 900 903 913 934 -hsync +vsync
-xrandr --addmode VGA1 1656x900_60.00
+xrandr --addmode VGA-1 1656x900_60.00
