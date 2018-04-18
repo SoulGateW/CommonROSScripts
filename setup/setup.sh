cadbe44ad11857fc6d3d67d45e0e55ad85271ab2
优里 <SoulCrash@yandex.ru>
2018-04-18 16:33:07 +0800

    Added functions, cleaned up.

---
+++ setup/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Initial setup for ROS Scripts, this script must be launched once to create
+# "config" folder and puopulate it with necessary files.
+
+mkdir "../config"
+echo "http://127.0.0.1:11311" > "../config/master_uri.conf"
+echo "127.0.0.1" > "../config/ros_ip.conf"
+echo "Configuration complete."
--- sshd.sh
+++ sshd.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Specific script. Sets resolution of second screen to support large values.
+# Used for SUNVIEW TV.
 
 xrandr --newmode "1656x900_60.00" 122.25 1656 1752 1920 2184 900 903 913 934 -hsync +vsync
 xrandr --addmode VGA-1 1656x900_60.00
